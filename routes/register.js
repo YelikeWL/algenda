f7b357de9d6dcfc5384628ea0eab1fd528c24a2e
Blazed <samueltheofie@gmail.com>
2022-04-16 15:12:43 +0700

    Added credential checking

--- routes/register.js
+++ routes/register.js
@@ -10,16 +10,47 @@ router.get('/', authentication.checkNotAuthenticated, (req, res) => {
 })
 
 router.post('/', authentication.checkNotAuthenticated, async (req, res) => {
+    const { name, email, password, confirm_password } = req.body
+    let errors = []
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        // To change when we update with database
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
+        // Authenticate credentials for registration
+        if(!name || !email || !password || !confirm_password) {
+            errors.push({ msg: 'Please enter all fields' });
+        }
+
+        if(password != confirm_password) {
+            errors.push({ msg: 'Passwords do not match' })
+        }
+
+        if(password.length < 6) {
+            errors.push({ msg: 'Password must be at least 6 characters' });
+        }
+
+        if(users.find({ email: email })) {
+            errors.push({ msg : 'Email already exists' })
+        }
+
+        if(errors.length > 0) {
+            res.render('register.ejs', {
+                errors,
+                name,
+                email,
+                password,
+                confirm_password
+            })
+        } else {
+            
+
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            // To change when we update with database
+            users.push({
+                id: Date.now().toString(),
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            })
+            res.redirect('/login')
+        }
     } catch {
         res.redirect('/')
     }
